6a7bfafc8182385da377bfd899d591aa95b09df2
blake Wu <limtty@163.com>
2021-12-14 20:03:04 +0800

    test add

--- src/main/test/ApplicationTest.java
+++ src/main/test/ApplicationTest.java
@@ -1,10 +1,11 @@
 package example.smallest;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 
 public class ApplicationTest{
     @Test
     public static void main(String[] args) {
     	System.out.println("This is just for test demo.") 
+        assertEquals(1,1);
 }
