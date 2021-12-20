e95c5f01f630ded0c24e25109678c4efe3dc8533
blake Wu <limtty@163.com>
2021-12-20 18:12:50 +0800

    add

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ cd ~&&
 sudo apt-get install default-jre -y &&
 
 #install tomcat
+sudo rm -rf /opt/apache-tomcat-8.5.73 /opt/tomcat8
 wget https://dlcdn.apache.org/tomcat/tomcat-8/v8.5.73/bin/apache-tomcat-8.5.73.tar.gz &&
 tar zxvf apache-tomcat-8.5.73.tar.gz &&
 sudo mv apache-tomcat-8.5.73/ /opt/apache-tomcat-8.5.73 &&
