ef4f3906e7ef6e36dc33871967b854e5590e2e68
blake Wu <limtty@163.com>
2021-12-20 19:16:21 +0800

    add

--- deploy.sh
+++ deploy.sh
@@ -9,5 +9,6 @@ sudo rm -rf /opt/apache-tomcat-8.5.73 /opt/tomcat8
 wget https://dlcdn.apache.org/tomcat/tomcat-8/v8.5.73/bin/apache-tomcat-8.5.73.tar.gz &&
 tar zxvf apache-tomcat-8.5.73.tar.gz &&
 sudo mv apache-tomcat-8.5.73/ /opt/apache-tomcat-8.5.73 &&
+sudo chown -R blake /opt/apache-tomcat-8.5.73 &&
 sudo ln -s /opt/apache-tomcat-8.5.73/ /opt/tomcat8 &&
 /opt/tomcat8/bin/startup.sh
